cfcece6ba9c85c81d3d385501dadcf46b4fbb4d6
vineethreddy <vineethg508@gmail.com>
2019-08-21 16:41:34 +0530

    after json testing and few other testings along with services

--- app/ServiceExample.js
+++ app/ServiceExample.js
@@ -1,6 +1,6 @@
 app.service('fakeData', function($http) {
     var data;
-    async: this.myFunc = function () {
+    this.myFunc = function () {
       
       $http.get('https://jsonplaceholder.typicode.com/todos/1').then(function (response) {
          
--- app/myCtrl.js
+++ app/myCtrl.js
@@ -1,13 +1,21 @@
-app.controller("myCtrl", function(fakeData,$scope) {
+app.controller("myCtrl", function(fakeData,$rootScope,$scope) {
   $scope.firstName = "John";
   $scope.lastName= "Doe";
   $scope.salary = 1024467;
   $scope.color = 'red';
-  $scope.buttonName = 'Click to hide'
+  $scope.buttonName = 'Click to hide';
+
+  var a ={"data" :[
+    {"name":"vineeth","age":25},{"name":"jyo","age":24}]
+  } ;
+  
+  $scope.vineeth = a.data;
+
+  
   $scope.getMessages = function()
   {
     var reddy = ['vineeth','gundeti','reddy'];
-
+    $rootScope.reddy1 = "vineethgundeti";
     return reddy;
 }
   $scope.clickChange =  true
@@ -24,6 +32,15 @@ app.controller("myCtrl", function(fakeData,$scope) {
 $scope.getServiceData = function()
    {
      
-     return fakeData.myFunc();
-   } 
+     if($scope.enable){
+   $scope.enable = false;
+   $scope.serviceData =  "";
+  }
+   else{ 
+     $scope.enable = true;
+   var s = fakeData.myFunc();
+    $scope.serviceData =  s;
+  }
+   }
+  
 });
